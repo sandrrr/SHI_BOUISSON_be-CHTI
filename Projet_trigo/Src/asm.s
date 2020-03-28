3c5a84f8199a7bb822e30784c2448f11977f72d9
Shi Mengxia <mshi@srv-neo2>
2020-03-28 17:59:01 +0100

    Use MLA

--- Projet_trigo/Src/asm.s
+++ Projet_trigo/Src/asm.s
@@ -16,7 +16,7 @@ module_carre proc
 	ldr r2, =TabCos ; r2 = @TabCos
 	ldr r3, =TabSin ; r3 = @TabSin
 	ldr r4, =N ; r4 = N
-	mov r5, #0 ; r5 = i
+	movw r5, #0 ; r5 = i
 	mov r6, #0 ; r6 = somme_reel
 	mov r7, #0 ; r7 = somme_imaginaire
 
@@ -26,18 +26,15 @@ for
 	ldrsh r9, [r2, r8, lsl #1] ; r9 = cos(ik%N)
 	ldrsh r10, [r3, r8, lsl #1] ; r10 = sin(ik%N)
 	ldrsh r11, [r0, r5, lsl #1] ; r11 = x(i)
-	mul r9, r11
-	mul r10, r11
+	mla r6, r9, r11, r6
+	mla r7, r10, r11, r7
 	
-	add r6, r9
-	add r7, r10
 	add r5, #1
-	
 	cmp r5, r4
 	bls for ; r5 <= r4
 	
 	smull r11, r6, r6, r6 ; r11,r6 = r6*r6 = Re(k)^2
-	smlal r11, r6, r7, r7 ; (r11,r7 += r7*r7) = Re(k)^2 + Im(k)^2
+	smlal r11, r6, r7, r7 ; (r11,r6 += r7*r7) = Re(k)^2 + Im(k)^2
 	mov r0, r6 
 	pop { r4, r5, r6, r7, r8, r9, r10, r11, pc }
 	endp
